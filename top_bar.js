b9de238117a04a8c1f09717f874f70331e039757
Roman Kurski <kwujciak@o2.pl>
2024-01-15 20:48:11 +0100

    First commit

---
+++ top_bar.js
@@ -0,0 +1,20 @@
+var top_bar = (v) => {
+    if (v == null) throw new Error("it's a null");
+    return v;
+}
+
+function injectCode(src) {
+    const script = document.createElement('script');
+    // This is why it works!
+    script.src = src;
+    script.onload = function() {
+        this.remove();
+    };
+
+    // This script runs before the <head> element is created,
+    // so we add the script to <html> instead.
+    top_bar(document.head || document.documentElement).appendChild(script);
+}
+
+
+injectCode(chrome.runtime.getURL('/top_bar1.js'));
